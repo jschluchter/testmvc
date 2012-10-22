3ec526eff313d5d0fea129eda320f6ad397b02b5
Joey Schluchter <jschluchter@gmail.com>
2012-10-22 15:19:54 -0400

    updating paths again

---
+++ testmvc/Controllers/DemoController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TestAzure.Azure.Tables;
+
+namespace testmvc.Controllers
+{
+    public class DemoController : Controller
+    {
+        //
+        // GET: /Demo/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public  ActionResult Sellers()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public  ActionResult Sellers(Seller model)
+        {
+            return View(model);
+        }
+
+    }
+}
